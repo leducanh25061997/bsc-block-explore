5f551c3c88be2aee22d5f389b9c350d3b92911fc
Waebox Media <waeboxmedia@Waeboxs-MacBook-Pro.local>
2025-08-23 09:24:56 +0700

    update api

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,1 +1,2 @@
-export const API_URL = "https://strong-adaptation-production.up.railway.app";
+export const API_URL = "https://powerful-vitality-production.up.railway.app";
+// export const API_URL = "https://strong-adaptation-production.up.railway.app";
